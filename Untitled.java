a1d46847d11c1ca020196b1c0614189ee6b56fd7
umabhuvaneshwarikv <uma.bhvaneshwari@cygeniq.com>
2026-01-07 12:30:19 +0530

    My changes for langfuse POC

---
+++ Untitled.java
@@ -0,0 +1,2 @@
+from langfuse import get_client
+ImportError: cannot import name 'get_client' from 'langfuse' (/home/spurge/Langfuse/langfuse_poc/venv/lib/python3.13/site-packages/langfuse/__init__.py)
